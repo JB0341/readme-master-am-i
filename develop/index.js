6669f7f29190e9d06750cf817e60ec59553b7bcb
j b <j.is.brown@gmail.com>
2024-07-01 14:37:10 -0500

    big commit. added validate to index.js. also built  render functions for the license badge, link, and section. started building the generate markdown function.

--- develop/index.js
+++ develop/index.js
@@ -1,48 +1,67 @@
 console.log('boomSAUCE');
 
+const fs = require('fs');
 const colors = require('colors');
 const inquirer = require ('inquirer');
-
+const generateMarkdown = require('./develope/generateMarkdown.js');
+//used inquirer to genterate questions
 inquirer
 .prompt([
     {
         type: "input",
         name: "title",
         message: "What is the title of your README?",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
         type: "input",
         name: "description",
         message: "Give me a brief description of your project.",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
         type: "input",
         name: "installation",
         message: "Do you have any installation instructions for potential users?",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
         type: "input",
         name: "usage",
         message: "Usage.",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
         type: "input",
         name: "Contributing",
         message: "How would you like others to contribute?",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
         type: "input",
         name: "Tests",
         message: "Any testing Guidelines?",
+        validate: (value)=>{ if(value){return true} else {return 'please input value to continue!'}}
     },
     {
-        type: "input",
+        type: "list",
         name: "License",
         message: "Choose your license.",
+        choices:['The MIT License', 'The GPL License', 'Apache license', 'GNU license'],
+        validate: (value)=>{ if(value){return true} else {return 'please select license to continue!'}} 
     }
 ])
-.then((response) =>
-console.log(response));
+.then(({
+    title,
+    description,
+    installation,
+    usage,
+    contributing,
+    tests,
+    license
+})=>{
+    const questions = 
+})
 
 
 
